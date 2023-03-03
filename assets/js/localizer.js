899093e0c2678ffbd8c859b283a434a2f1ee9aa2
Gil Serrancolí <gil.serrancoli@upc.edu>
2023-03-03 21:52:28 +0100

    Changes text, video and organization

--- assets/js/localizer.js
+++ assets/js/localizer.js
@@ -12,7 +12,7 @@ var dictionary = {
         "Research": "Research",
         "Meet the team": "Meet the team",
         "News": "News",
-        "Get in touch": "Get in touch",
+        "Contact": "Contact",
 
         /*Buttons*/
         "Learn More": "Learn more",
@@ -32,11 +32,11 @@ var dictionary = {
         "About Section Rehab Info": "Gamified telerehabilitation system aimed at subjects with neuromuscular disorders",
         "About Section Customizable Info": "The therapist adapts the treatment according to your evolution remotely",
         "About Section Clinical Relevant Data Info": "3D capture, kinematics and dynamics analyses to improve physiotherapy outcomes",
-        "About Section Flexible Info": "Muvity adapats the goals of the exergames according to the capabilities of the user",
+        "About Section Flexible Info": "Muvity adapts the goals of the exergames according to the capabilities of the user",
 
         /*Research*/
         "Research Title": "Research",
-        "Research Paragraph1": "In the following section you can take a deeper look on the papers and research this initiative is based on.",
+        "Research Paragraph1": "In the following section you can take a deeper look on the research this initiative is based on.",
         /*Sections*/
         "Research Section1 Title": "A feasibility study to assess the effectiveness of Muvity: A telerehabilitation system for chronic post-stroke subjects",
         "Research Section1 Info": "A feasibility cross-over analysis was conducted in ten chronic post-stroke subjects. Two randomized groups followed two eight-weeks treatments, one with the telerehabilitation system Muvity and the other following conventional therapy (in random order). Before and after each treatment, physical evaluations were performed assessing functional independence, the perceived level of pain, balance control and self-reported health status. After the study, the participants answered a short questionnaire to measure the usability of the system.",
@@ -47,7 +47,7 @@ var dictionary = {
 
         /*Meet the team*/
         "Meet The Team Title": "Meet the Team",
-        "Meet The Team Paragraph1": "Behind Muvity there is a complex and throrough team with expertise in various fields to develop to the best of our capabilities, with researchers from the UPC and physiotherapists form ADFO.",
+        "Meet The Team Paragraph1": "Behind Muvity there is a multidiscipliary team with expertise in various fields, with researchers from the UPC and therapists from ADFO.",
 
         /*News*/
         "News Title": "News",
@@ -59,9 +59,9 @@ var dictionary = {
         "News Section3 Title": "",
         "News Section3 Info": "",
 
-        /*Get In Touch*/
-        "Get In Touch Title": "Get in touch",
-        "Get In Touch Paragraph1": "If you want to learn more about what we do or if you would like to collaborate, please do contact us.",
+        /*Contact*/
+        "Contact Title": "Contact",
+        "Contact Paragraph1": "If you want to learn more about what we do or if you would like to collaborate, please do contact us.",
         /*Form*/
         "Form Contact": "Contact",
         "Form First Name": "First Name",
@@ -69,9 +69,9 @@ var dictionary = {
         "Form Email": "Email",
         "Form Send Message": "Send Message",
         /*Info*/
-        "Get In Touch Address": "Address",
-        "Get In Touch Email": "Email",
-        "Get In Touch Social": "Social",
+        "Contact Address": "Address",
+        "Contact Email": "Email",
+        "Contact Social": "Social",
     },
 
 // ENGLISH
@@ -86,7 +86,7 @@ var dictionary = {
         "Research": "Research",
         "Meet the team": "Meet the team",
         "News": "News",
-        "Get in touch": "Get in touch",
+        "Contact": "Contact",
 
         /*Buttons*/
         "Learn More": "Learn more",
@@ -121,7 +121,7 @@ var dictionary = {
 
         /*Meet the team*/
         "Meet The Team Title": "Meet the Team",
-        "Meet The Team Paragraph1": "Behind Muvity there is a complex and throrough team with expertise in various fields to develop to the best of our capabilities, with researchers from the UPC and physiotherapists form ADFO.",
+        "Meet The Team Paragraph1": "Behind Muvity there is a multidiscipliary team with expertise in various fields, with researchers from the UPC and therapists from ADFO.",
 
         /*News*/
         "News Title": "News",
@@ -133,9 +133,9 @@ var dictionary = {
         "News Section3 Title": "",
         "News Section3 Info": "",
 
-        /*Get In Touch*/
-        "Get In Touch Title": "Get in touch",
-        "Get In Touch Paragraph1": "If you want to learn more about what we do or if you would like to collaborate, please do contact us.",
+        /*Contact*/
+        "Contact Title": "Contact",
+        "Contact Paragraph1": "If you want to learn more about what we do or if you would like to collaborate, please do contact us.",
         /*Form*/
         "Form Contact": "Contact",
         "Form First Name": "First Name",
@@ -143,9 +143,9 @@ var dictionary = {
         "Form Email": "Email",
         "Form Send Message": "Send Message",
         /*Info*/
-        "Get In Touch Address": "Address",
-        "Get In Touch Email": "Email",
-        "Get In Touch Social": "Social",
+        "Contact Address": "Address",
+        "Contact Email": "Email",
+        "Contact Social": "Social",
     },
 
 //CATALÀ
@@ -160,7 +160,7 @@ var dictionary = {
         "Research": "Investigació",
         "Meet the team": "Coneix l'equip",
         "News": "Notícies",
-        "Get in touch": "Posa't en contacte",
+        "Contact": "Posa't en contacte",
 
         /*Buttons*/
         "Learn More": "Saber-ne més",
@@ -174,20 +174,20 @@ var dictionary = {
         "About Paragraph1": "Muvity és una aplicació de rehabilitació que promou la participació activa de l'usuari en la seva pròpia rehabilitació, i és una eina per als fisioterapeutes per a fer el seguiment dels seus pacients.",
         /*Sections*/
         "About Section Rehab": "Rehabilitació",
-        "About Section Customizable": "Customizable",
+        "About Section Customizable": "Personalitzable",
         "About Section Clinical Relevant Data": "Dades Clínicament Rellevants",
         "About Section Flexible": "Flexible",
-        "About Section Rehab Info": "Sistema de rehabilitació gamificat dirigit a persones amb problemes neuromusculars",
+        "About Section Rehab Info": "Sistema de rehabilitació gamificat dirigit a persones amb lesions neuromusculars",
         "About Section Customizable Info": "El fisioterapeuta adapta el tractament segons la teva evolució remotament",
-        "About Section Clinical Relevant Data Info": "Captura 3D, anàlisi cinemàtica i dinàmica per a millorar els resultats de la fisioteràpia",
-        "About Section Flexible Info": "Muvity adapta les metes dels jocs segons les capacitats de l'usuari",
+        "About Section Clinical Relevant Data Info": "Captura 3D, anàlisis cinemàtiques i dinàmiques per a millorar els resultats de la fisioteràpia",
+        "About Section Flexible Info": "Muvity adapta els objectius dels jocs segons les capacitats de l'usuari",
 
         /*Research*/
         "Research Title": "Investigació",
-        "Research Paragraph1": "En la següent secció podeu fer un cop d'ull als articles en els quals es basa aquesta iniciativa.",
+        "Research Paragraph1": "En la següent secció es pot conèixer la recerca en la qual es basa aquesta iniciativa.",
         /*Sections*/
         "Research Section1 Title": "Estudi de viabilitat per avaluar l'eficàcia de Muvity: un sistema de telerehabilitació per a subjectes crònics després d'un ictus",
-        "Research Section1 Info": "Es va realitzar una anàlisi creuada de viabilitat en deu subjectes crònics després d'un ictus. Dos grups aleatoris van seguir dos tractaments de vuit setmanes, un amb el sistema de telerehabilitació Muvity i l'altre després de la teràpia convencional (en ordre aleatori). Abans i després de cada tractament, es van realitzar avaluacions físiques avaluant la independència funcional, el nivell de dolor percebut, el control de l'equilibri i l'estat de salut autoinformat. Després de l'estudi, els participants van respondre un breu qüestionari per mesurar la usabilitat del sistema.",
+        "Research Section1 Info": "Es va realitzar una anàlisi de viabilitat creuada en deu subjectes crònics després d'un ictus. Dos grups aleatoris van seguir dos tractaments de vuit setmanes, un amb el sistema de telerehabilitació Muvity i l'altre després de la teràpia convencional (en ordre aleatori). Abans i després de cada tractament, es van realitzar avaluacions físiques avaluant la independència funcional, el nivell de dolor percebut, el control de l'equilibri i l'estat de salut autoinformat. Després de l'estudi, els participants van respondre un breu qüestionari per mesurar la usabilitat del sistema.",
         "Research Section2 Title": "",
         "Research Section2 Info": "",
         "Research Section3 Title": "",
@@ -207,9 +207,9 @@ var dictionary = {
         "News Section3 Title": "",
         "News Section3 Info": "",
 
-        /*Get In Touch*/
-        "Get In Touch Title": "Posa't en contacte",
-        "Get In Touch Paragraph1": "Si vols saber més sobre què fem o si vols col·laborar, posa't en contacte amb nosaltres.",
+        /*Contact*/
+        "Contact Title": "Posa't en contacte",
+        "Contact Paragraph1": "Si vols saber més sobre què fem o si vols col·laborar, posa't en contacte amb nosaltres.",
         /*Form*/
         "Form Contact": "Contacte",
         "Form First Name": "Nom",
@@ -217,9 +217,9 @@ var dictionary = {
         "Form Email": "Email",
         "Form Send Message": "Enviar missatge",
         /*Info*/
-        "Get In Touch Address": "Direcció",
-        "Get In Touch Email": "Email",
-        "Get In Touch Social": "Xarxes Socials",
+        "Contact Address": "Direcció",
+        "Contact Email": "Email",
+        "Contact Social": "Xarxes Socials",
     },
 
 //ESPAÑOL
@@ -234,7 +234,7 @@ var dictionary = {
         "Research": "Investigación",
         "Meet the team": "Conoce al equipo",
         "News": "Noticias",
-        "Get in touch": "Ponte en contacto",
+        "Contact": "Ponte en contacto",
 
         /*Buttons*/
         "Learn More": "Saber más",
@@ -258,7 +258,7 @@ var dictionary = {
 
         /*Research*/
         "Research Title": "Investigación",
-        "Research Paragraph1": "En la siguiente sección podéis echar una ojeada a los artículos en los cuales se basa esta iniciativa.",
+        "Research Paragraph1": "En la siguiente sección podéis conocer la investigación en la cual se basa esta iniciativa.",
         /*Sections*/
         "Research Section1 Title": "Estudio de viabilidad para evaluar la eficacia de Muvity: un sistema de telerehabilitación para sujetos crónicos que han sufrido un ictus",
         "Research Section1 Info": "Se realizó un análisis cruzado de factibilidad en diez sujetos crónicos que sufrieron un accidente cerebrovascular. Dos grupos aleatorizados siguieron dos tratamientos de ocho semanas, uno con el sistema de telerehabilitación Muvity y el otro siguiendo la terapia convencional (en orden aleatorio). Antes y después de cada tratamiento, se realizaron evaluaciones físicas evaluando la independencia funcional, el nivel de dolor percibido, el control del equilibrio y el estado de salud autoinformado. Después del estudio, los participantes respondieron un breve cuestionario para medir la usabilidad del sistema.",
@@ -274,16 +274,16 @@ var dictionary = {
         /*News*/
         "News Title": "Noticias",
         /*Sections*/
-        "News Section1 Title": "Published article",
+        "News Section1 Title": "Artículo publicado",
         "News Section1 Info": "Evaluar la viabilidad de un sistema de telerehabilitación para sujetos crónicos postictus frente a un tratamiento convencional.",
         "News Section2 Title": "",
         "News Section2 Info": "",
         "News Section3 Title": "",
         "News Section3 Info": "",
 
-        /*Get In Touch*/
-        "Get In Touch Title": "Ponte en contacto",
-        "Get In Touch Paragraph1": "Si quieres saber más sobre lo que hacemos o si te gustaría colaborar, ponte en contacto con nosotros.",
+        /*Contact*/
+        "Contact Title": "Ponte en contacto",
+        "Contact Paragraph1": "Si quieres saber más sobre lo que hacemos o si te gustaría colaborar, ponte en contacto con nosotros.",
         /*Form*/
         "Form Contact": "Contacto",
         "Form First Name": "Nombre",
@@ -291,9 +291,9 @@ var dictionary = {
         "Form Email": "Email",
         "Form Send Message": "Enviar mensaje",
         /*Info*/
-        "Get In Touch Address": "Dirección",
-        "Get In Touch Email": "Email",
-        "Get In Touch Social": "Redes sociales",
+        "Contact Address": "Dirección",
+        "Contact Email": "Email",
+        "Contact Social": "Redes sociales",
     }
 
 }
